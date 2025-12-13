afd08072e94b5a0dc153d0eba51d1d0f4ffffe48
Garry-QD <804252316@qq.com>
2025-12-14 00:54:39 +0800

    fix: increase json body limit to 120mb

--- server/server.js
+++ server/server.js
@@ -211,8 +211,8 @@ async function getDefaultData() {
 ensureInit();
 
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "120mb" }));
+app.use(express.urlencoded({ extended: true, limit: "120mb" }));
 
 // Helper to get user file path
 function getUserFile(username) {
